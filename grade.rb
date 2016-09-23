10e64ee7a38075cc7d2ab76b16092df943a4f462
Nina Wilson <nina.wilson.mlis@gmail.com>
2016-09-23 19:55:06 -0400

    Last commit

--- grade.rb
+++ grade.rb
@@ -4,17 +4,17 @@ score = gets.chomp.to_i
 
 
     if   score >= 101
-    	    print "Wrong Answer!"
-    elsif  score >= 90
-         puts "Your Grade is a A"
-    elsif  score >= 80
-         puts "Your Grade is a B"
-    elsif  score >= 70
-         puts "Your Grade is a C"
-    elsif  score >= 60
-         puts "Your Grade is a D"
+    	    puts "Wrong Answer!"
+    elsif  score >= 90 && score <= 100
+         puts "With a score of #{score}, Your Grade is a A"
+    elsif  score >= 80 && score <= 89
+         puts "With a score of #{score},Your Grade is a B"
+    elsif  score >= 70 && score <= 79
+         puts "With a score of #{score},Your Grade is a C"
+    elsif  score >= 60 && score <= 69
+         puts "With a score of #{score},Your Grade is a D"
     else   
-    	   puts "Your Grade is an F"
+    	   puts "With a score of #{score},Your Grade is an F"
 
     end
 
